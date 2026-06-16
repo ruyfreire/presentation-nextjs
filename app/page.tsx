cdfc0d2560f4146a7178d9ddd183e479d0180ce5
Ruy Freire <ruy.slv@gmail.com>
2026-06-16 18:55:43 -0300

    Refactor loading state and add initial loading animation
    - Updated the Home component to use the new InitialLoading component for better user experience during data fetching.
    - Replaced the previous loading animation with a more visually appealing SVG animation.
    - Improved error handling by displaying an error message and image when profile data fails to load.
    - Enhanced the loading state logic for clarity and maintainability.

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 'use client'
 
-import { motion, Variants } from 'motion/react'
+import { AnimatePresence, motion } from 'motion/react'
+import Image from 'next/image'
 import { useEffect, useState } from 'react'
 
 import { Hero } from '@/components/hero'
+import { InitialLoading } from '@/components/initial-loading'
 import { Section } from '@/components/section'
 import { useGetProfile } from '@/services/get-profile'
 import { formatDate } from '@/utils/formatters'
 
 export default function Home() {
-  const { data: response, isLoading } = useGetProfile()
+  const { data: response, isLoading: isLoadingProfile } = useGetProfile()
   const [delayInitializing, setDelayInitializing] = useState(true)
 
   const formatDateRange = ({
@@ -32,90 +34,86 @@ export default function Home() {
     }, 3000)
   }, [])
 
-  if (isLoading || delayInitializing) {
-    const dotVariants: Variants = {
-      pulse: {
-        scale: [1, 1.5, 1],
-        transition: {
-          duration: 0.8,
-          repeat: Infinity,
-          ease: 'easeInOut',
-        },
-      },
-    }
-
-    return (
-      <motion.div
-        animate="pulse"
-        transition={{ staggerChildren: -0.2, staggerDirection: -1 }}
-        className="w-screen h-screen flex items-center justify-center fixed z-50 gap-4"
-      >
-        <motion.div
-          className="bg-muted size-8 rounded-full"
-          variants={dotVariants}
-        />
-        <motion.div
-          className="bg-muted size-8 rounded-full"
-          variants={dotVariants}
-        />
-        <motion.div
-          className="bg-muted size-8 rounded-full"
-          variants={dotVariants}
-        />
-      </motion.div>
-    )
-  }
-
-  if (!response) {
-    return <div>Não foi possível carregar o perfil</div>
-  }
-
-  const profile = response.data
+  const isLoading = isLoadingProfile || delayInitializing
+  const profile = response?.data
 
   return (
     <div className="relative w-full py-14 flex gap-10 flex-col items-center">
-      <Hero profile={profile} />
+      <AnimatePresence>
+        {isLoading ? (
+          <InitialLoading />
+        ) : !!profile ? (
+          <motion.div
+            className=" flex w-full gap-10 flex-col items-center"
+            key="profile"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.4, ease: 'easeInOut' }}
+          >
+            <Hero profile={profile} />
 
-      <Section title="Sobre">
-        <p className="text-justify">{profile.bio}</p>
-      </Section>
+            <Section title="Sobre">
+              <p className="text-justify">{profile.bio}</p>
+            </Section>
 
-      <Section title="Experiência profissional">
-        <Section.List>
-          {profile.experiences.map((experience) => (
-            <Section.Item
-              key={experience.id}
-              date={formatDateRange({
-                startDate: experience.startDate,
-                endDate: experience.endDate,
-              })}
-              title={experience.role}
-              subtitle={experience.company}
-              description={experience.description}
-              tags={experience.tags}
+            <Section title="Experiência profissional">
+              <Section.List>
+                {profile.experiences.map((experience) => (
+                  <Section.Item
+                    key={experience.id}
+                    date={formatDateRange({
+                      startDate: experience.startDate,
+                      endDate: experience.endDate,
+                    })}
+                    title={experience.role}
+                    subtitle={experience.company}
+                    description={experience.description}
+                    tags={experience.tags}
+                  />
+                ))}
+              </Section.List>
+            </Section>
+
+            <Section title="Formação">
+              <Section.List>
+                {profile.education.map((education) => (
+                  <Section.Item
+                    key={education.id}
+                    date={formatDateRange({
+                      startDate: education.startDate,
+                      endDate: education.endDate,
+                    })}
+                    title={education.title}
+                    subtitle={education.institution}
+                    description={education.description}
+                    tags={education.tags}
+                    certificateUrl={education.certificateUrl}
+                  />
+                ))}
+              </Section.List>
+            </Section>
+          </motion.div>
+        ) : (
+          <div className="flex fixed top-0 left-0 w-full h-full bg-muted flex-col gap-4 items-center justify-center">
+            <Image
+              src="/found-error.png"
+              alt="Erro ao carregar o perfil"
+              className="dark:bg-white p-2 rounded-md"
+              width={150}
+              height={150}
             />
-          ))}
-        </Section.List>
-      </Section>
 
-      <Section title="Formação">
-        <Section.List>
-          {profile.education.map((education) => (
-            <Section.Item
-              key={education.id}
-              date={formatDateRange({
-                startDate: education.startDate,
-                endDate: education.endDate,
-              })}
-              title={education.title}
-              subtitle={education.institution}
-              description={education.description}
-              tags={education.tags}
-              certificateUrl={education.certificateUrl}
-            />
-          ))}
-        </Section.List>
-      </Section>
+            <div className="text-center">
+              <h1 className="text-xl font-bold">
+                Ops, Erro ao carregar o perfil
+              </h1>
+              <p className="text-sm text-muted-foreground">
+                Tente novamente mais tarde.
+              </p>
+            </div>
+          </div>
+        )}
+      </AnimatePresence>
     </div>
   )
 }
