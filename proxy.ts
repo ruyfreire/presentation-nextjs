f693238df51c4ef36a30a6a9b7d6584e7d7510db
Ruy Freire <ruy.slv@gmail.com>
2026-06-18 18:13:15 -0300

    Refactor proxy handling and update VSCode settings for Prettier integration
    - Simplified the `proxy.ts` file by removing unnecessary line breaks for cleaner code.
    - Enhanced `.vscode/settings.json` to include Prettier as a code action on save for improved formatting consistency.
    - Adjusted `axios.ts` to standardize string quotes for better readability.

--- proxy.ts
+++ proxy.ts
@@ -7,15 +7,15 @@ export function proxy(request: NextRequest) {
     requestHeaders.set('Authorization', process.env.API_TOKEN)
   }
 
-  const urlBackend = request.nextUrl.pathname.replace('/api', '');
-  const urlDestino = new URL(urlBackend, process.env.NEXT_PUBLIC_API_URL);
-  urlDestino.search = request.nextUrl.search;
+  const urlBackend = request.nextUrl.pathname.replace('/api', '')
+  const urlDestino = new URL(urlBackend, process.env.NEXT_PUBLIC_API_URL)
+  urlDestino.search = request.nextUrl.search
 
   return NextResponse.rewrite(urlDestino, {
     request: {
       headers: requestHeaders,
     },
-  });
+  })
 }
 
 export const config = {
