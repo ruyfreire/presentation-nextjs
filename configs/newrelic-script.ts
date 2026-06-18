30bc9d52cd5dc946cfc51abca2537aaf9116ccee
Ruy Freire <ruy.slv@gmail.com>
2026-06-18 14:40:16 -0300

    Add Prettier format fix script and update New Relic script formatting
    - Added a new script command `format:fix` to `package.json` for automatic code formatting with Prettier.
    - Updated the formatting of the New Relic script in `configs/newrelic-script.ts` for improved readability.

--- configs/newrelic-script.ts
+++ configs/newrelic-script.ts
@@ -1,6 +1,6 @@
 import { readFileSync } from 'node:fs'
 import { join } from 'node:path'
 export const newRelicScript = readFileSync(
-    join(process.cwd(), 'configs/newrelic-script.txt'),
-    'utf8',
+  join(process.cwd(), 'configs/newrelic-script.txt'),
+  'utf8',
 )
