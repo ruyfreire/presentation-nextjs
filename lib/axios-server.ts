da7ba201fad979b7027ed6328a28492e5a761714
Ruy Freire <ruy.slv@gmail.com>
2026-06-23 19:26:08 -0300

    Add EditorConfig for consistent coding styles and format axios-server.ts

--- lib/axios-server.ts
+++ lib/axios-server.ts
@@ -3,10 +3,10 @@
 import axios from 'axios'
 
 const serverApi = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL,
-    headers: {
-        Authorization: process.env.API_TOKEN ?? '',
-    },
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    Authorization: process.env.API_TOKEN ?? '',
+  },
 })
 
 export { serverApi }
