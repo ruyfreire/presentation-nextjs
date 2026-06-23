8159b8aef5b09da43f024be6f35ef07319e5d832
Ruy Freire <ruy.slv@gmail.com>
2026-06-23 19:33:32 -0300

    Refactor SuspenseLoading component for improved layout and styling
    - Adjusted the layout of the `SuspenseLoading` component to center items and limit maximum width.
    - Updated skeleton loading elements for better visual consistency and responsiveness.

--- components/suspense-loading.tsx
+++ components/suspense-loading.tsx
@@ -30,15 +30,15 @@ const SectionSkeleton = () => {
 
 export function SuspenseLoading() {
   return (
-    <div className="w-full min-h-screen z-50 opacity-80 absolute top-0 left-0 flex flex-col py-14 px-4">
-      <div className="flex flex-col gap-10">
+    <div className="w-full min-h-screen z-50 absolute top-0 left-0 flex flex-col py-14 px-4 items-center">
+      <div className="flex w-full flex-col gap-10 max-w-4xl">
         <div className="flex gap-4 py-2">
           {/* Imagem */}
-          <Skeleton className="size-24 rounded-full" />
+          <Skeleton className="size-24 shrink-0 rounded-full" />
 
           <div className="flex flex-col gap-2 flex-1 py-2">
-            <Skeleton className="w-56 h-7 rounded-md" />
-            <Skeleton className="w-72 h-4 rounded-md" />
+            <Skeleton className="w-44 h-7 rounded-md" />
+            <Skeleton className="w-56 h-4 rounded-md" />
             <Skeleton className="w-40 h-4 rounded-md" />
           </div>
         </div>
