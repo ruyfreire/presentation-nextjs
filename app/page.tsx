391f78b781afb46519dead1a6847899f8dad6ad1
Ruy Freire <ruy.slv@gmail.com>
2026-09-06 16:22:30 -0300

    feat: refactor profile loading into Home component
    - Moved profile loading logic from the Profile component to the Home component for better organization.
    - Implemented loading states and error handling directly in Home, enhancing user experience.
    - Removed the Profile component as it is no longer needed, streamlining the codebase.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,145 @@
-import { Profile } from '@/components/profile'
+'use client'
+
+import { isAxiosError } from 'axios'
+import { useEffect, useRef, useState } from 'react'
+
+import { Container } from '@/components/container'
+import { Hero } from '@/components/hero'
+import { InitialLoading } from '@/components/initial-loading'
+import { LoadingDialog } from '@/components/loading-dialog'
+import { NavigateButton } from '@/components/navigate-button'
+import { ProfileErrorMessage } from '@/components/profile-error-message'
+import { Section } from '@/components/section'
+import { useGetProfile } from '@/services/get-profile'
+import { formatDate } from '@/utils/formatters'
+
+const TIME_LIMIT = 5_000 // 5 seconds in milliseconds
 
 export default function Home() {
-  return <Profile />
+  const startTime = useRef<number | null>(null)
+  const [loadingTime, setLoadingTime] = useState(false)
+
+  const {
+    data: response,
+    isLoading: isLoadingProfile,
+    isError,
+    failureReason,
+  } = useGetProfile()
+
+  const formatDateRange = ({
+    startDate,
+    endDate,
+  }: {
+    startDate: unknown
+    endDate?: unknown
+  }) => {
+    if (endDate) {
+      return `${formatDate(startDate, 'yyyy')} - ${formatDate(endDate, 'yyyy')}`
+    }
+
+    return formatDate(startDate, 'yyyy')
+  }
+
+  useEffect(() => {
+    startTime.current = Date.now()
+
+    const interval = setInterval(() => {
+      if (startTime.current) {
+        if (Date.now() - startTime.current > TIME_LIMIT) {
+          setLoadingTime(true)
+          clearInterval(interval)
+        }
+      }
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [])
+
+  const profile = response?.data
+  const isNotFound =
+    isAxiosError(failureReason) && failureReason?.status === 404
+  const isLoading = isLoadingProfile
+  const isLoadingTime = loadingTime && isLoading && !profile
+
+  if (isError && !isNotFound) {
+    return (
+      <ProfileErrorMessage
+        image="/error.png"
+        title="Ops, Erro ao carregar o perfil!"
+        description="Tente novamente mais tarde."
+      />
+    )
+  }
+
+  if (isNotFound || (!isLoading && !profile)) {
+    return (
+      <ProfileErrorMessage
+        image="/not-found.png"
+        title="Ops, Perfil não encontrado!"
+        description="Não foi possível encontrar o perfil solicitado."
+      />
+    )
+  }
+
+  return (
+    <>
+      {isLoading ? (
+        <>
+          <InitialLoading />
+          {isLoadingTime && <LoadingDialog />}
+        </>
+      ) : !!profile ? (
+        <div className=" flex gap-10 flex-col items-center">
+          <Hero profile={profile} />
+
+          <Container>
+            <Section title="Sobre">
+              <p className="text-justify whitespace-pre-line">{profile.bio}</p>
+            </Section>
+
+            <Section title="Experiência profissional">
+              <Section.List>
+                {profile.experiences.map((experience) => (
+                  <Section.Item
+                    key={experience.id}
+                    date={formatDateRange({
+                      startDate: experience.startDate,
+                      endDate: experience.endDate,
+                    })}
+                    title={experience.role}
+                    subtitle={experience.company}
+                    description={experience.description}
+                    tags={experience.tags}
+                  />
+                ))}
+              </Section.List>
+            </Section>
+
+            <Section title="Formação">
+              <Section.List>
+                {profile.education.map((education) => (
+                  <Section.Item
+                    key={education.id}
+                    date={formatDateRange({
+                      startDate: education.startDate,
+                      endDate: education.endDate,
+                    })}
+                    title={education.title}
+                    subtitle={education.institution}
+                    description={education.description}
+                    tags={education.tags}
+                    certificateUrl={education.certificateUrl}
+                  />
+                ))}
+              </Section.List>
+            </Section>
+          </Container>
+        </div>
+      ) : (
+        <p>Carregando...</p>
+      )}
+
+      <NavigateButton href="/about" />
+    </>
+  )
 }
